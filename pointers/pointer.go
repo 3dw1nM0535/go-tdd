6f5fdcff1db215cc5778a5bf7c2f2a066796d6d3
3dw1nM0535 <workockmoses@gmail.com>
2020-03-05 13:27:37 +0300

    add comments block

--- pointers/pointer.go
+++ pointers/pointer.go
@@ -6,8 +6,10 @@ import (
 	"errors"
 )
 
+// Bitcoin : type bitcoin
 type Bitcoin int
 
+// Stringer : interface for printing to the StdOut
 type Stringer interface {
 	String() string
 }
@@ -16,20 +18,25 @@ func (b Bitcoin) String() string {
 	return fmt.Sprintf("%d BTC", b)
 }
 
+// Wallet : type wallet
 type Wallet struct {
 	balance Bitcoin
 }
 
+// Deposit : add funds to your account
 func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
 }
 
+// Balance : returns the account balance
 func (w *Wallet) Balance() Bitcoin {
 	return w.balance
 }
 
+// ErrorInsufficientBalance : error message for insufficient funds during withdrawal
 var ErrorInsufficientBalance = errors.New("Oh no! Insufficient balance in your account to initiate widthrawal")
 
+// Widthraw : widthraw cash from the account
 func (w *Wallet) Widthraw(amount Bitcoin) error {
 	if amount > w.balance {
 		return ErrorInsufficientBalance
